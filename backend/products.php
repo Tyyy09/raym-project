53ad3a209224d92e6f4560e810df9341b13c3322
Tyyy09 <164542500+Tyyy09@users.noreply.github.com>
2025-12-02 21:11:31 -0500

    final sneakerlisrt products.php
    fetch sneakers from database and display in styled table with Edit/Delete actions.

--- backend/products.php
+++ backend/products.php
@@ -1,1 +1,39 @@
 <?php
+include 'Database.php';
+$conn = Database::getInstance()->getConnection();
+
+$query = "SELECT * FROM sneakers ORDER BY created_at DESC";
+$stmt = $conn->prepare($query);
+$stmt->execute();
+$sneakers = $stmt->fetchAll();
+?>
+
+<h1 class="sneaker-title">Sneaker List</h1>
+<a href="add_product.php" class="btn-add">Add Sneaker</a>
+
+<table class="sneaker-table">
+    <thead>
+    <tr>
+        <th class="col-id">ID</th>
+        <th class="col-name">Name</th>
+        <th class="col-brand">Brand</th>
+        <th class="col-price">Price</th>
+        <th class="col-actions">Actions</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($sneakers as $sneaker): ?>
+        <tr>
+            <td><?php echo htmlspecialchars($sneaker['id']); ?></td>
+            <td><?php echo htmlspecialchars($sneaker['name']); ?></td>
+            <td><?php echo htmlspecialchars($sneaker['brand']); ?></td>
+            <td>$<?php echo htmlspecialchars($sneaker['price']); ?></td>
+            <td>
+                <a href="update_product.php?id=<?php echo $sneaker['id']; ?>" class="btn-edit">Edit</a>
+                <a href="delete_product.php?id=<?php echo $sneaker['id']; ?>" class="btn-delete">Delete</a>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+
